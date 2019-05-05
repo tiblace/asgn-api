27ba97cfc2c63278297ce8843abe5e270d62536d
Sheryl Selby-Spear <48160556+tiblace@users.noreply.github.com>
2019-05-05 10:51:23 -0500

    adding asgn model

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
---
+++ asgn-model.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var AsgnSchema = new Schema({
+    courseName: String,
+    assignmentName: {
+        type: String,
+        required: true 
+    },
+    dueDate: Date,
+});
+
+module.exports = mongoose.model('asgn', AsgnSchema);
